318ee0588a0647878d78ae6085823a7b4071635f
David McClure <david.mcclure@virginia.edu>
2013-05-21 11:46:41 -0400

    Committing generated static sites.

---
+++ prompt_book/_site/javascripts/plugin.js
@@ -0,0 +1,28 @@
+// Avoid `console` errors in browsers that lack a console
+if (!(window.console && console.log)) {
+    (function() {
+        var noop = function() {};
+        var methods = ['assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error', 'exception', 'group', 'groupCollapsed', 'groupEnd', 'info', 'log', 'markTimeline', 'profile', 'profileEnd', 'markTimeline', 'table', 'time', 'timeEnd', 'timeStamp', 'trace', 'warn'];
+        var length = methods.length;
+        var console = window.console = {};
+        while (length--) {
+            console[methods[length]] = noop;
+        }
+    }());
+}
+
+// place any jQuery/helper plugins in here, instead of separate, slower script files.
+
+
+$(function() {
+  //$('#com3269').modal();
+ // $('.fragment').click(function(event) {
+   //event.preventDefault();
+
+    //$(this).popover({
+      //title: 'title',
+      //content: $(this).attr('href')
+    //});
+ // });
+
+});
